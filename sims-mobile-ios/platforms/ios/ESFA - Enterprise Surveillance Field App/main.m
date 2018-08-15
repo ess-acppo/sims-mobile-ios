ba561cb590d34109633b8a90c5ebc1acdbdccf27
Rajendra Jonnalagedda <jonnalageddaraj@gmail.com>
2018-08-15 10:12:08 +1000

    DataDirectory option for iOS

--- sims-mobile-ios/platforms/ios/ESFA - Enterprise Surveillance Field App/main.m
+++ sims-mobile-ios/platforms/ios/ESFA - Enterprise Surveillance Field App/main.m
@@ -18,7 +18,7 @@
  */
 //
 //  main.m
-//  SIMS Mobile
+//  ESFA - Enterprise Surveillance Field App
 //
 //  Created by ___FULLUSERNAME___ on ___DATE___.
 //  Copyright ___ORGANIZATIONNAME___ ___YEAR___. All rights reserved.
