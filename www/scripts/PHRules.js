9c15becc5b7a4adacc91c3bde497abac0f665f89
Rajendra Jonnalagedda <jonnalageddaraj@gmail.com>
2018-10-03 09:41:39 +1000

    updated uat urls

--- www/scripts/PHRules.js
+++ www/scripts/PHRules.js
@@ -43,7 +43,9 @@ function syncPHRefCodes() {
     var settings = {
         "async": false,
         "crossDomain": true,
-        "url": "https://online-dev.agriculture.gov.au/ords-int/rest/sims/plant_health/referenceCodes",
+        //"url": "https://online-dev.agriculture.gov.au/ords-int/rest/sims/plant_health/referenceCodes",
+        //"url": "https://online-sit.agriculture.gov.au/ords-int/rest/sims/plant_health/referenceCodes",
+        "url": "https://online-uat.agriculture.gov.au/ords-int/rest/sims/plant_health/referenceCodes",
         "method": "GET",
         "beforeSend": function () {
             $('#mb6 .progText').text("Syncing Reference Codes ...");
@@ -163,7 +165,9 @@ function syncActivityData() {
     var settings = {
         "async": false,
         "crossDomain": true,
-        "url": "https://online-dev.agriculture.gov.au/ords-int/rest/sims/plant_health/activity",
+        //"url": "https://online-dev.agriculture.gov.au/ords-int/rest/sims/plant_health/activity",
+        //"url": "https://online-sit.agriculture.gov.au/ords-int/rest/sims/plant_health/activity",
+        "url": "https://online-uat.agriculture.gov.au/ords-int/rest/sims/plant_health/activity",
         "method": "GET",
         "beforeSend": function () {
             $('#mb6 .progText').text("Syncing Activity Data ...");
@@ -244,7 +248,9 @@ function syncstaffData() {
     var NPHsettings = {
         "async": false,
         "crossDomain": true,
-        "url": "https://online-dev.agriculture.gov.au/ords-int/rest/sims/common/program/NPH/team",
+        //"url": "https://online-dev.agriculture.gov.au/ords-int/rest/sims/common/program/NPH/team",
+        //"url": "https://online-sit.agriculture.gov.au/ords-int/rest/sims/common/program/NPH/team",
+        "url": "https://online-uat.agriculture.gov.au/ords-int/rest/sims/common/program/NPH/team",
         "method": "GET",
         "beforeSend": function () {
             $('#mb6 .progText').text("Syncing NPH Staff Data ...");
@@ -292,7 +298,9 @@ function syncBPHstaffData() {
     var BPHsettings = {
         "async": false,
         "crossDomain": true,
-        "url": "https://online-dev.agriculture.gov.au/ords-int/rest/sims/common/program/BPH/team",
+        //"url": "https://online-dev.agriculture.gov.au/ords-int/rest/sims/common/program/BPH/team",
+        //"url": "https://online-sit.agriculture.gov.au/ords-int/rest/sims/common/program/BPH/team",
+        "url": "https://online-uat.agriculture.gov.au/ords-int/rest/sims/common/program/BPH/team",
         "method": "GET",
         "beforeSend": function () {
             $('#mb6 .progText').text("Syncing BPH Staff Data ...");
@@ -339,7 +347,9 @@ function syncIPHstaffData() {
     var IPHsettings = {
         "async": false,
         "crossDomain": true,
-        "url": "https://online-dev.agriculture.gov.au/ords-int/rest/sims/common/program/IPH/team",
+        //"url": "https://online-dev.agriculture.gov.au/ords-int/rest/sims/common/program/IPH/team",
+        //"url": "https://online-sit.agriculture.gov.au/ords-int/rest/sims/common/program/IPH/team",
+        "url": "https://online-uat.agriculture.gov.au/ords-int/rest/sims/common/program/IPH/team",
         "method": "GET",
         "beforeSend": function () {
             $('#mb6 .progText').text("Syncing IPH Staff Data ...");
@@ -396,7 +406,9 @@ function syncTaxaData() {
     var Taxasettings = {
         "async": false,
         "crossDomain": true,
-        "url": "https://online-dev.agriculture.gov.au/ords-int/rest/sims/plant_health/taxa",
+        //"url": "https://online-dev.agriculture.gov.au/ords-int/rest/sims/plant_health/taxa",
+        //"url": "https://online-sit.agriculture.gov.au/ords-int/rest/sims/plant_health/taxa",
+        "url": "https://online-uat.agriculture.gov.au/ords-int/rest/sims/plant_health/taxa",
         "method": "GET",
         "beforeSend": function () {
             $('#mb6 .progText').text("Syncing Taxa ...");
