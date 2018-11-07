e25f9d9653c924d82197268d51284688229dc8c1
Rajendra Jonnalagedda <jonnalageddaraj@gmail.com>
2018-11-08 10:24:01 +1100

    changes to InitJS

--- www/scripts/_init.js
+++ www/scripts/_init.js
@@ -3126,6 +3126,7 @@ $(document).on('click', 'a.downloadMaps', function (e) {
             db.transaction(function (tx) {
                 tx.executeSql("UPDATE settings SET settingsval = ? WHERE id = ?", [JSON.stringify(resSettings), 1], function (tx, res) {
                     $('#form3').find('label.mapNotes').text(new Date().toString());
+                    //$('#modalProgress').modal('hide');
                 });
             }, function (err) {
                 $.growl({ title: "", message: "An error occured while updating mapsets. " + err.message, location: "tc", size: "large" });
