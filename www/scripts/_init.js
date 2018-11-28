6eecb283290c0141dbc29fd90ed1899f055be941
Rajendra Jonnalagedda <jonnalageddaraj@gmail.com>
2018-11-28 19:09:55 +1100

    extobs and backup restore try fix

--- www/scripts/_init.js
+++ www/scripts/_init.js
@@ -3038,31 +3038,17 @@ function exportTableToCSV($table, filename) {
 }
 function backupDatabase() {
     var fileName = cordova.file.applicationStorageDirectory + 'Library/LocalDatabase/sims.db';
-    var fileName2 = cordova.file.dataDirectory + 'backup/backup.db';    
     var directoryName = cordova.file.dataDirectory;
-    window.resolveLocalFileSystemURL(fileName2, function (fileEntry1) { 
-        window.resolveLocalFileSystemURL(fileName, function (fileEntry) {
-            window.resolveLocalFileSystemURL(directoryName, function (directoryEntry) {
-                directoryEntry.getDirectory("backup", { create: true, exclusive: false }, function (bkupdirectoryEntry) {
-                    fileEntry1.remove(function () {
-                        fileEntry.copyTo(bkupdirectoryEntry, "backup.db", function (cpfileEntry) {
-                            $.when(fetchSettings()).then(initSettings()).done(function () {
-                                $.growl.notice({ title: "", message: "Observations backedup to local Backup folder.", location: "tc", size: "large" });
-                            });                    
-                        });                                        
-                    }, function (error) {
-                        // Error deleting the file
-                        $.growl.error({ title: "", message: "Error removing db file.", location: "tc", size: "large" });
-                    }, function () {
-                        // The file doesn't exist
-                        $.growl.notice({ title: "", message: "DB file does not exist.", location: "tc", size: "large" });
-                    });                     
-                }, function (error) {
-                });
-            }, function (error) {
+    window.resolveLocalFileSystemURL(fileName, function (fileEntry) {
+        window.resolveLocalFileSystemURL(directoryName, function (directoryEntry) {
+            directoryEntry.getDirectory("backup", { create: true, exclusive: false }, function (bkupdirectoryEntry) {
+                    fileEntry.copyTo(bkupdirectoryEntry, name, function (cpfileEntry) {
+                        $.when(fetchSettings()).then(initSettings()).done(function () {
+                            $.growl.notice({ title: "", message: "Observations backedup to local Backup folder.", location: "tc", size: "large" });
+                    });                    
+                });                    
             });
-        }, function (error) {
-        });        
+        });
     });
 }
 function restoreDatabase() {
@@ -3071,31 +3057,20 @@ function restoreDatabase() {
          content: 'Do you want to restore from backup? You may lose the observations that were recorded after the last backup!',
          buttons: {
              Ok: function () {
-                 var fileName = cordova.file.dataDirectory + 'backup/backup.db';
-                 var fileName2 = cordova.file.applicationDirectory + 'Library/LocalDatabase/sims.db';
-                 window.resolveLocalFileSystemURL(fileName2, function (fileEntry1) {
-                     window.resolveLocalFileSystemURL(fileName, function (fileEntry) {
-                        window.resolveLocalFileSystemURL(cordova.file.applicationStorageDirectory, function (directoryEntry) {
-                            directoryEntry.getDirectory("Library", { create: false, exclusive: false }, function (bkupdirectoryEntry1) {
-                                bkupdirectoryEntry1.getDirectory("LocalDatabase", { create: false, exclusive: false }, function (bkupdirectoryEntry) {
-                                    fileEntry1.remove(function () {
-                                        fileEntry.copyTo(bkupdirectoryEntry, "sims.db", function (cpfileEntry) {
-                                            $.when(fetchSettings()).then(initSettings()).done(function () {
-                                                $.growl({ title: "", message: "Observations restored to the application.", location: "tc", size: "large"});
-                                            });
-                                        });                                          
-                                    }, function (error) {
-                                        // Error deleting the file
-                                        $.growl.error({ title: "", message: "Error removing db file.", location: "tc", size: "large" });
-                                    }, function () {
-                                        // The file doesn't exist
-                                        $.growl.notice({ title: "", message: "DB file does not exist.", location: "tc", size: "large" });
-                                    });                                                                  
-                                });
-                            });                                        
-                        });
-                     });                          
-                 });                           
+                var fileName = cordova.file.dataDirectory + 'backup/sims.db';
+                window.resolveLocalFileSystemURL(fileName, function (fileEntry) {
+                    window.resolveLocalFileSystemURL(cordova.file.applicationStorageDirectory, function (directoryEntry) {
+                        directoryEntry.getDirectory("Library", { create: false, exclusive: false }, function (bkupdirectoryEntry1) {
+                            bkupdirectoryEntry1.getDirectory("LocalDatabase", { create: false, exclusive: false }, function (bkupdirectoryEntry) {
+                                fileEntry.copyTo(bkupdirectoryEntry, name, function (cpfileEntry) {
+                                    $.when(fetchSettings()).then(initSettings()).done(function () {
+                                        $.growl({ title: "", message: "Observations restored to the application.", location: "tc", size: "large"});
+                                    });
+                                });                                                                                                   
+                            });
+                        });                                        
+                    });
+                 });                          
             },
              cancel: function () {
             }
