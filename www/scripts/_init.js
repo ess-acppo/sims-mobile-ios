0ffed749e1e832839302b3e13beede07c7587c39
Rajendra Jonnalagedda <jonnalageddaraj@gmail.com>
2019-01-15 13:41:32 +1100

    Issue fixes 1218,1227,1267

--- www/scripts/_init.js
+++ www/scripts/_init.js
@@ -8,6 +8,7 @@ var ServerAddress;
 var devServerAddress;
 var sitServerAddress;
 var uatServerAddress;
+var preProdServerAddress;
 var prodServerAddress;
 var authAddress;
 var ActivityAddress;
@@ -2597,6 +2598,10 @@ function fetchServerDetails(serverMode, appMode) {
     devServerAddress = resSettings.settings.app.devServerAddress;
     sitServerAddress = resSettings.settings.app.sitServerAddress;
     uatServerAddress = resSettings.settings.app.uatServerAddress;
+    if (!resSettings.settings.app.preProdServerAddress) {
+        preProdServerAddress = "https://online-ppr.agriculture.gov.au/";
+        resSettings.settings.app.preProdServerAddress = "https://online-ppr.agriculture.gov.au/";
+    } else { preProdServerAddress = resSettings.settings.app.preProdServerAddress; }       
     prodServerAddress = resSettings.settings.app.prodServerAddress;
     switch (appMode) {
         case "PH":
@@ -2634,6 +2639,17 @@ function fetchServerDetails(serverMode, appMode) {
                     taxaAddress = ServerAddress + resSettings.settings.app.taxaAddress;
                     submitPHObsAddress = ServerAddress + resSettings.settings.app.submitPHObsAddress;
                     break;
+                case "PREPROD":
+                    ServerAddress = preProdServerAddress;
+                    authAddress = ServerAddress + resSettings.settings.app.authAddress;
+                    ActivityAddress = ServerAddress + resSettings.settings.app.activityAddress;
+                    refCodesAddress = ServerAddress + resSettings.settings.app.refCodesAddress;
+                    BPHStaffAddress = ServerAddress + resSettings.settings.app.BPHStaffAddress;
+                    IPHStaffAddress = ServerAddress + resSettings.settings.app.IPHStaffAddress;
+                    NPHStaffAddress = ServerAddress + resSettings.settings.app.NPHStaffAddress;
+                    taxaAddress = ServerAddress + resSettings.settings.app.taxaAddress;
+                    submitPHObsAddress = ServerAddress + resSettings.settings.app.submitPHObsAddress;
+                    break;                    
                 case "PROD":
                     ServerAddress = prodServerAddress;
                     authAddress = ServerAddress + resSettings.settings.app.authAddress;
@@ -2679,6 +2695,16 @@ function fetchServerDetails(serverMode, appMode) {
                     submitAHFAObsAddress = ServerAddress + resSettings.settings.app.submitAHFAObsAddress;
                     submitAHGObsAddress = ServerAddress + resSettings.settings.app.submitAHGObsAddress;
                     break;
+                case "PREPROD":
+                    ServerAddress = preProdServerAddress;
+                    authAddress = ServerAddress + resSettings.settings.app.authAddressAH;
+                    ActivityAddressAH = ServerAddress + resSettings.settings.app.activityAddressAH;
+                    refCodesAddressAH = ServerAddress + resSettings.settings.app.refCodesAddressAH;
+                    NAFStaffAddress = ServerAddress + resSettings.settings.app.NAFStaffAddress;
+                    speciesTaxonSyndromSamplesAddress = ServerAddress + resSettings.settings.app.speciesTaxonSyndromSamplesAddress;
+                    submitAHFAObsAddress = ServerAddress + resSettings.settings.app.submitAHFAObsAddress;
+                    submitAHGObsAddress = ServerAddress + resSettings.settings.app.submitAHGObsAddress;
+                    break;                    
                 case "PROD":
                     ServerAddress = prodServerAddress;
                     authAddress = ServerAddress + resSettings.settings.app.authAddressAH;
